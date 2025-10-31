fee0b42a06601a386cb40ea633d72aee2480aeec
Jesus Hernandez <jhernandez4598@sdsu.edu>
2025-10-31 13:42:31 -0700

    Added upheap

--- heap.h
+++ heap.h
@@ -20,19 +20,18 @@ struct MinHeap
         // Avoid overflow
         if (size > 63) return;
 
-        data[size++] = idx;
-
-        upheap(size - 1, weightArr);
+        data[size++] = idx;              // Add to end
+        upheap(size - 1, weightArr); // and then heapify
     }
 
     int pop(int weightArr[])
     {
+        // Avoid underflow
         if (size == 0) return -1;
 
-        int minIdx = data[0];
-
-        if (--size > 0)
-        {
+        int minIdx = data[0]; // Save the root
+        if (--size > 0)       // and then set root to last
+        {                     // and then heapify
             data[0] = data[size];
             downheap(0, weightArr);
         }
@@ -42,7 +41,21 @@ struct MinHeap
 
     void upheap(int pos, int weightArr[])
     {
-        // TODO: swap child upward while smaller than parent
+        if (pos < 0 || size < 2) return; // OOB or zero/one node
+        if (pos >= size) pos = size - 1; // OOB, default to last
+
+        int parent = (pos - 1) / 2;
+        while (weightArr[data[pos]] < weightArr[data[parent]])
+        {
+            // Swap idx at pos and parent if pos < parent
+            int temp     = data[pos];
+            data[pos]    = data[parent];
+            data[parent] = temp;
+
+            // Set child and parent
+            pos = parent;
+            parent = (pos - 1) / 2;
+        }
     }
 
     void downheap(int pos, int weightArr[])
